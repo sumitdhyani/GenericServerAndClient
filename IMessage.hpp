324c4c71f2e6dac2ae06b90d9660ec0a8c02e0e9
U-IONTRADING\Sumit.Dhyani <Sumit.Dhyani@IONDELLT259.iontrading.com>
2020-08-25 11:16:22 +0530

    Added callback in the send method

--- IMessage.hpp
+++ IMessage.hpp
@@ -6,6 +6,8 @@ namespace ULCommunicationFramework
 	template<class RawMsgType>
 	struct IMessage
 	{
-		virtual void send(RawMsgType, IMsgPropagator<RawMsgType>) = 0;
+		typedef IMsgPropagator<RawMsgType> IMsgPropagator;
+		DEFINE_PTR(IMsgPropagator)
+		virtual void send(RawMsgType, IMsgPropagator_SPtr, std::function<void(bool)>) = 0;
 	};
 }
